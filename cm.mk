54109bf985d1fc4a5cdda7efe5008cf828abfa78
sanja56 <sanja5682@mail.ru>
2017-03-14 00:34:14 +0300

    first commit

---
+++ cm.mk
@@ -0,0 +1,27 @@
+# Release name
+PRODUCT_RELEASE_NAME := m350
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/ixion/m350/device_m350.mk)
+
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+
+TARGET_SCREEN_HEIGHT := 1280	
+TARGET_SCREEN_WIDTH := 720
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m350
+PRODUCT_NAME := cm_m350
+PRODUCT_BRAND := ixion
+PRODUCT_MODEL := m350
+PRODUCT_MANUFACTURER := dexp
+
+PRODUCT_DEFAULT_LANGUAGE := ru
+PRODUCT_DEFAULT_REGION   := RU
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.timezone=Europe/Moscow
